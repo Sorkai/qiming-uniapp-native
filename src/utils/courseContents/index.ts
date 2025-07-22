044649a353242aa743ee34ba2d8bcf2d3ffbd1b9
Kai Wang <wk@sorkai.com>
2025-07-22 23:38:24 +0800

    format

--- src/utils/courseContents/index.ts
+++ src/utils/courseContents/index.ts
@@ -1,5 +1,5 @@
-import { mathCourseContent } from './math';
-import { linuxCourseContent } from './linux';
+import { mathCourseContent } from "./math";
+import { linuxCourseContent } from "./linux";
 
 /**
  * 根据课程名称获取课程内容
@@ -8,10 +8,10 @@ import { linuxCourseContent } from './linux';
  */
 export function getCourseContentByName(courseName: string): string {
   const courseMap: Record<string, string> = {
-    '高等数学': mathCourseContent,
-    '嵌入式Linux开发实践教程': linuxCourseContent,
+    高等数学: mathCourseContent,
+    嵌入式Linux开发实践教程: linuxCourseContent
     // 可以继续添加更多课程内容
   };
 
-  return courseMap[courseName] || '';
-} 
+  return courseMap[courseName] || "";
+}
