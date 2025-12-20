0185f77250ebf79e5be40d9a75c813aa99c077d6
冯傲然 <faremail2024@icloud.com>
2025-12-20 16:19:29 +0800

    commit

--- src/components/LoginDialog.vue
+++ src/components/LoginDialog.vue
@@ -1,9 +1,9 @@
 <template>
   <Teleport to="body">
-    <Transition name="modal">
+    <Transition name="modal">  
       <div v-if="visible" class="login-overlay" @click.self="emit('update:visible', false)">
         <div class="login-card" :class="{ shake: isShaking }">
-          <!-- 关闭按钮 -->
+          <!--   关闭按钮   -->
           <button class="close-btn" @click="emit('update:visible', false)">
             <svg viewBox="0 0 24 24" width="18" height="18">
               <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"/>
