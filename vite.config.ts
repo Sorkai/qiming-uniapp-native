7ada5f4fb2d1a2395ac969fb639dc85b41545feb
冯傲然 <faremail2024@icloud.com>
2026-01-12 15:45:28 +0800

    version 4.2.5 更改图片服务器代理

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,10 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
           target: "https://aiedu-file.lehinet.com",
           changeOrigin: true,
           rewrite: path => path.replace(/^\/file-proxy/, "")
+        },
+        "/ai-edu-bucket": {
+          target: "https://aiedu-file.lehinet.com",
+          changeOrigin: true
         }
       },
       // 预热文件以提前转换和缓存结果，降低启动期间的初始页面加载时长并防止转换瀑布
